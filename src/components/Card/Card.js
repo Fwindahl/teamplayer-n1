14f8f5e09e8db6a615e958d22c11f4daca363016
Fwindahl <filipwindahl@hotmail.com>
2022-06-08 17:26:31 +0200

    card frame and background done

---
+++ src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '././Card.css';
+
+const Card = () => {
+	return (
+		<div className="body">
+			<div className="card">
+				<div className="top-images">
+					<img className="dancing-hero" src="/images/illustration-hero.svg"></img>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
